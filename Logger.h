ad44730ddd1dcefa2ca63128e76f6b87c0b41adf
supermicro <supermicro@scnu.com>
2023-09-11 22:51:18 +0800

    理顺自己的项目，添加更多说明

--- Logger.h
+++ Logger.h
@@ -5,52 +5,53 @@
 #include "noncopyable.h"
 
 // LOG_INFO("%s %d", arg1, arg2)
-#define LOG_INFO(logmsgFormat, ...) \
-    do \
-    { \
-        Logger &logger = Logger::instance(); \
-        logger.setLogLevel(INFO); \
-        char buf[1024] = {0}; \
+// 如果忘了这一段代码的含义可以丢到GPT里面去
+#define LOG_INFO(logmsgFormat, ...)                       \
+    do                                                    \
+    {                                                     \
+        Logger &logger = Logger::instance();              \
+        logger.setLogLevel(INFO);                         \
+        char buf[1024] = {0};                             \
         snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
-        logger.log(buf); \
-    } while(0) 
+        logger.log(buf);                                  \
+    } while (0)
 
-#define LOG_ERROR(logmsgFormat, ...) \
-    do \
-    { \
-        Logger &logger = Logger::instance(); \
-        logger.setLogLevel(ERROR); \
-        char buf[1024] = {0}; \
+#define LOG_ERROR(logmsgFormat, ...)                      \
+    do                                                    \
+    {                                                     \
+        Logger &logger = Logger::instance();              \
+        logger.setLogLevel(ERROR);                        \
+        char buf[1024] = {0};                             \
         snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
-        logger.log(buf); \
-    } while(0) 
+        logger.log(buf);                                  \
+    } while (0)
 
-#define LOG_FATAL(logmsgFormat, ...) \
-    do \
-    { \
-        Logger &logger = Logger::instance(); \
-        logger.setLogLevel(FATAL); \
-        char buf[1024] = {0}; \
+#define LOG_FATAL(logmsgFormat, ...)                      \
+    do                                                    \
+    {                                                     \
+        Logger &logger = Logger::instance();              \
+        logger.setLogLevel(FATAL);                        \
+        char buf[1024] = {0};                             \
         snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
-        logger.log(buf); \
-        exit(-1); \
-    } while(0) 
+        logger.log(buf);                                  \
+        exit(-1);                                         \
+    } while (0)
 
 #ifdef MUDEBUG
-#define LOG_DEBUG(logmsgFormat, ...) \
-    do \
-    { \
-        Logger &logger = Logger::instance(); \
-        logger.setLogLevel(DEBUG); \
-        char buf[1024] = {0}; \
+#define LOG_DEBUG(logmsgFormat, ...)                      \
+    do                                                    \
+    {                                                     \
+        Logger &logger = Logger::instance();              \
+        logger.setLogLevel(DEBUG);                        \
+        char buf[1024] = {0};                             \
         snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
-        logger.log(buf); \
-    } while(0) 
+        logger.log(buf);                                  \
+    } while (0)
 #else
-    #define LOG_DEBUG(logmsgFormat, ...)
+#define LOG_DEBUG(logmsgFormat, ...)
 #endif
 
-// 定义日志的级别  INFO  ERROR  FATAL  DEBUG 
+// 定义日志的级别  INFO  ERROR  FATAL  DEBUG
 enum LogLevel
 {
     INFO,  // 普通信息
@@ -63,12 +64,9 @@ enum LogLevel
 class Logger : noncopyable
 {
 public:
-    // 获取日志唯一的实例对象
-    static Logger& instance();
-    // 设置日志级别
-    void setLogLevel(int level);
-    // 写日志
-    void log(std::string msg);
+    static Logger &instance();   // 获取日志唯一的实例对象
+    void setLogLevel(int level); // 设置日志级别
+    void log(std::string msg);   // 写日志
 private:
     int logLevel_;
 };
--- README.md
+++ README.md
@@ -1,18 +1,67 @@
 # simpleMuduo
+
 A simplified Muduo network lib using C++ 11 features.
 
-# TODO
+* [X] 用C++ 11重写陈硕的muduo网络库，去掉对Boost库的依赖
+* [X] EchoServer实现、运行
+* [ ] HTTP Server实现、运行
 
-1. 继续完善注释
-2. ~~研究如何在Linux服务器上克隆当前仓库~~并运行案例
-3. 测试EchoServer是否能正常运行
+# simpleMuduo网络库的运行原理
 
+* [ ] 如何理解Reactor网络模型？
+* [ ] 各个模块都是干嘛的？
+* [ ] 一个客户连接来了后会发生什么？
 
-# updateLogs
+## 模块解析
 
-## 2023-08-25
+* [X] noncopyable
+* [ ] Logger
+* [ ] Timestamp
+* [ ] InetAddress
+* [ ] Channel
+* [ ] Poller
+* [ ] EpollPoller
+* [ ] EventLoop
+* [ ] Thread
+* [ ] EventLoopThread
+* [ ] Socket
+* [ ] Acceptor
+* [ ] TcpServer
 
-1. 上传代码
-2. 已经注释了一部分
+### noncopyable
 
+noncopyable被继承以后，派生类对象可以正常的构造和析构，但是派生类对象无法进行拷贝构造和赋值操作，通过 `= delete` 实现，有点像单例模式？感觉就是啊
 
+### Logger
+
+定义了几个宏来写不同级别的日志：
+
+* LOG_INFO
+* LOG_ERROR
+* LOG_FATAL
+* LOG_DEBUG
+
+只要看懂下面这个代码就能读懂
+
+```C++
+// LOG_INFO("%s %d", arg1, arg2)
+#define LOG_INFO(logmsgFormat, ...)                       \
+    do                                                    \
+    {                                                     \
+        Logger &logger = Logger::instance();              \
+        logger.setLogLevel(INFO);                         \
+        char buf[1024] = {0};                             \
+        snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
+        logger.log(buf);                                  \
+    } while (0)
+```
+
+## 多Reactor 多线程
+
+这里有一个问题后续再明确一下，就是SubReactor是否会将业务处理交给WorkThread
+
+![img](image/README/1694440902198.png)
+
+这个[项目](https://github.com/Shangyizhou/A-Tiny-Network-Library)里的示意图是有画的是有工作线程池的
+
+![img](https://camo.githubusercontent.com/43f02acdbd589ba7df40fdd0a7890a47314fc8d08e831f5f22c6f0414d0acd4d/68747470733a2f2f63646e2e6e6c61726b2e636f6d2f79757175652f302f323032322f706e672f32363735323037382f313637303835333133343532382d63383864323766322d313061322d343664332d623330382d3438663736333261326630392e706e673f782d6f73732d70726f636573733d696d616765253246726573697a65253243775f3933372532436c696d69745f30)
