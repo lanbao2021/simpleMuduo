8832addcf0180acf46690c7b6405c886f946eb9e
supermicro <supermicro@scnu.com>
2023-10-22 15:55:46 +0800

    完善Channel类的注释

--- Channel.cc
+++ Channel.cc
@@ -10,25 +10,26 @@ const int Channel::kWriteEvent = EPOLLOUT;
 
 // EventLoop: ChannelList Poller
 Channel::Channel(EventLoop *loop, int fd)
-    : loop_(loop), fd_(fd), events_(0), revents_(0), index_(-1), tied_(false)
-{
-}
+    : loop_(loop),
+      fd_(fd),
+      events_(0),
+      revents_(0),
+      index_(-1),
+      tied_(false) {}
 
-Channel::~Channel()
-{
-}
+Channel::~Channel() {}
 
-// channel的tie方法什么时候调用过？一个TcpConnection新连接创建的时候 TcpConnection => Channel 
+// channel的tie方法什么时候调用过？一个TcpConnection新连接创建的时候 TcpConnection => Channel
 void Channel::tie(const std::shared_ptr<void> &obj)
 {
-    tie_ = obj;
-    tied_ = true;
+    tie_ = obj;   // 保存TcpConnection对象的弱引用
+    tied_ = true; // 标记已经绑定了TcpConnection对象
 }
 
 /**
  * 当改变channel所表示fd的events事件后，update负责在poller里面更改fd相应的事件epoll_ctl
  * EventLoop => ChannelList   Poller
- */ 
+ */
 void Channel::update()
 {
     // 通过channel所属的EventLoop，调用poller的相应方法，注册fd的events事件
@@ -43,65 +44,66 @@ void Channel::remove()
 
 /**
  * @brief 处理channel上的活跃事件
- * 
+ *
  * 当和TcpConnection对象绑定了，说明是client的EventLoop，那么先检查和TcpConnection之间的绑定关系是否正常
- * 
+ *
  * 如果tied_为true，那么进一步检测tie_.lock()是否能成功将weak_ptr -> shared_ptr，失败的话就没必要继续往下执行handleEventWithGuard咯
- * 
+ *
  * 如果压根没有tied说明是acceptor对应的EventLoop，所以直接handleEventWithGuard（如果我没理解错的话）
- * 
- * @param receiveTime 
+ *
+ * @param receiveTime
  */
 void Channel::handleEvent(Timestamp receiveTime)
 {
-    if (tied_)
+    if (tied_) // 如果channel绑定了TcpConnection对象
     {
-        std::shared_ptr<void> guard = tie_.lock();
-        if (guard)
+        std::shared_ptr<void> guard = tie_.lock(); // 将weak_ptr转换为shared_ptr
+
+        if (guard) // 如果转换成功
         {
-            handleEventWithGuard(receiveTime);
+            handleEventWithGuard(receiveTime); // 处理channel上的活跃事件
         }
     }
-    else
+    else // 如果channel没有绑定TcpConnection对象
     {
-        handleEventWithGuard(receiveTime);
+        handleEventWithGuard(receiveTime); // 处理channel上的活跃事件
     }
 }
 
 // 根据poller通知的channel发生的具体事件， 由channel负责调用具体的回调操作
 void Channel::handleEventWithGuard(Timestamp receiveTime)
 {
-    LOG_INFO("channel handleEvent revents:%d\n", revents_);
+    LOG_INFO("channel handleEvent revents:%d\n", revents_); // 打印channel发生的具体事件
 
-    if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN))
+    if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) // 如果发生了EPOLLHUP事件，但是没有发生EPOLLIN事件
     {
         if (closeCallback_)
         {
-            closeCallback_();
+            closeCallback_(); // 调用closeCallback_回调操作
         }
     }
 
-    if (revents_ & EPOLLERR)
+    if (revents_ & EPOLLERR) // 如果发生了EPOLLERR事件
     {
         if (errorCallback_)
         {
-            errorCallback_();
+            errorCallback_(); // 调用errorCallback_回调操作
         }
     }
 
-    if (revents_ & (EPOLLIN | EPOLLPRI))
+    if (revents_ & (EPOLLIN | EPOLLPRI)) // 如果发生了EPOLLIN或者EPOLLPRI事件
     {
         if (readCallback_)
         {
-            readCallback_(receiveTime);
+            readCallback_(receiveTime); // 调用readCallback_回调操作，传入Timestamp对象!
         }
     }
 
-    if (revents_ & EPOLLOUT)
+    if (revents_ & EPOLLOUT) // 如果发生了EPOLLOUT事件
     {
         if (writeCallback_)
         {
-            writeCallback_();
+            writeCallback_(); // 调用writeCallback_回调操作 
         }
     }
 }
--- Channel.h
+++ Channel.h
@@ -12,18 +12,17 @@ class EventLoop;
  * 理清楚  EventLoop、Channel、Poller之间的关系   《= Reactor模型上对应 Demultiplex
  * Channel 理解为通道，封装了sockfd和其感兴趣的event，如EPOLLIN、EPOLLOUT事件
  * 还绑定了poller返回的具体事件
- */ 
+ */
 class Channel : noncopyable
 {
 public:
-    using EventCallback = std::function<void()>;
-    using ReadEventCallback = std::function<void(Timestamp)>;
+    using EventCallback = std::function<void()>;              // 回调函数对象
+    using ReadEventCallback = std::function<void(Timestamp)>; // 读事件回调函数对象
 
-    Channel(EventLoop *loop, int fd);
-    ~Channel();
+    Channel(EventLoop *loop, int fd); // 构造函数，初始化channel
+    ~Channel();                       // 析构函数，关闭fd
 
-    // fd得到poller通知以后，处理事件的
-    void handleEvent(Timestamp receiveTime);  
+    void handleEvent(Timestamp receiveTime); // fd得到poller通知以后，处理事件的
 
     // 设置回调函数对象
     void setReadCallback(ReadEventCallback cb) { readCallback_ = std::move(cb); }
@@ -31,53 +30,68 @@ public:
     void setCloseCallback(EventCallback cb) { closeCallback_ = std::move(cb); }
     void setErrorCallback(EventCallback cb) { errorCallback_ = std::move(cb); }
 
-    // 防止当channel被手动remove掉，channel还在执行回调操作
-    void tie(const std::shared_ptr<void>&);
+    void tie(const std::shared_ptr<void> &); // 设置channel绑定的TcpConnection对象
 
     int fd() const { return fd_; }
     int events() const { return events_; }
-    int set_revents(int revt) { revents_ = revt; }
+    int set_revents(int revt) { revents_ = revt; } // used by pollers
 
-    // 设置fd相应的事件状态
-    void enableReading() { events_ |= kReadEvent; update(); }
-    void disableReading() { events_ &= ~kReadEvent; update(); }
-    void enableWriting() { events_ |= kWriteEvent; update(); }
-    void disableWriting() { events_ &= ~kWriteEvent; update(); }
-    void disableAll() { events_ = kNoneEvent; update(); }
+    void enableReading() // 用于开启fd的读事件
+    {
+        events_ |= kReadEvent;
+        update();
+    }
+    void disableReading() // 用于关闭fd的读事件
+    {
+        events_ &= ~kReadEvent;
+        update();
+    }
+    void enableWriting() // 用于开启fd的写事件
+    {
+        events_ |= kWriteEvent;
+        update();
+    }
+    void disableWriting() // 用于关闭fd的写事件
+    {
+        events_ &= ~kWriteEvent;
+        update();
+    }
+    void disableAll() // 用于关闭fd所有的事件
+    {
+        events_ = kNoneEvent;
+        update();
+    }
 
-    // 返回fd当前的事件状态
-    bool isNoneEvent() const { return events_ == kNoneEvent; }
-    bool isWriting() const { return events_ & kWriteEvent; }
-    bool isReading() const { return events_ & kReadEvent; }
+    bool isNoneEvent() const { return events_ == kNoneEvent; } // 用于判断fd当前是否有事件发生
+    bool isWriting() const { return events_ & kWriteEvent; }   // 用于判断fd是否注册了写事件
+    bool isReading() const { return events_ & kReadEvent; }    // 用于判断fd是否注册了读事件
 
-    int index() { return index_; }
-    void set_index(int idx) { index_ = idx; }
+    int index() { return index_; }            // 获取channel在ChannelList中的索引位置
+    void set_index(int idx) { index_ = idx; } // 设置channel在ChannelList中的索引位置
+    EventLoop *ownerLoop() { return loop_; }  // 用于获取channel所属的EventLoop
+    void remove();                            // 从EventLoop中移除channel
 
-    // one loop per thread
-    EventLoop* ownerLoop() { return loop_; }
-    void remove();
 private:
+    void update(); // 更新channel所属的EventLoop监听的fd感兴趣的事件
 
-    void update();
-    void handleEventWithGuard(Timestamp receiveTime);
+    void handleEventWithGuard(Timestamp receiveTime); // 事件处理函数，由handleEvent()调用
 
-    static const int kNoneEvent;
-    static const int kReadEvent;
-    static const int kWriteEvent;
+    static const int kNoneEvent;  // 无事件
+    static const int kReadEvent;  // 读事件
+    static const int kWriteEvent; // 写事件
 
-    EventLoop *loop_; // 事件循环
+    EventLoop *loop_; // 指向所属的EventLoop，事件循环
     const int fd_;    // fd, Poller监听的对象
-    int events_; // 注册fd感兴趣的事件
-    int revents_; // poller返回的具体发生的事件
-    int index_;
+    int events_;      // 注册fd感兴趣的事件
+    int revents_;     // poller返回的具体发生的事件
+    int index_;       // used by Poller.  Poller监听的对象在ChannelList中的索引位置
 
-    std::weak_ptr<void> tie_;
-    bool tied_;
+    std::weak_ptr<void> tie_; // 保存TcpConnection对象的弱引用，防止TcpConnection对象被手动remove掉，channel还在执行回调操作
+    bool tied_;               // 是否绑定了TcpConnection对象
 
     // 因为channel通道里面能够获知fd最终发生的具体的事件revents，所以它负责调用具体事件的回调操作
-    ReadEventCallback readCallback_;
-    EventCallback writeCallback_;
-    EventCallback closeCallback_;
-    EventCallback errorCallback_;
+    ReadEventCallback readCallback_; // 读事件回调操作
+    EventCallback writeCallback_;    // 写事件回调操作
+    EventCallback closeCallback_;    // 关闭事件回调操作
+    EventCallback errorCallback_;    // 错误事件回调操作
 };
-
