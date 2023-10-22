fb83f83e7131dd4aa062946cd2e34772741cfccb
supermicro <supermicro@scnu.com>
2023-10-22 12:54:38 +0800

    InetAddress进一步注释

--- InetAddress.cc
+++ InetAddress.cc
@@ -5,34 +5,35 @@
 
 InetAddress::InetAddress(uint16_t port, std::string ip)
 {
-    bzero(&addr_, sizeof addr_);
-    addr_.sin_family = AF_INET;
-    addr_.sin_port = htons(port);
-    addr_.sin_addr.s_addr = inet_addr(ip.c_str());
+    bzero(&addr_, sizeof addr_);                   // 将 addr_ 清零
+    addr_.sin_family = AF_INET;                    // IPv4
+    addr_.sin_port = htons(port);                  // 端口号
+    addr_.sin_addr.s_addr = inet_addr(ip.c_str()); // IP 地址
 }
 
 std::string InetAddress::toIp() const
 {
-    // addr_
+    // The :: before the function name indicates that it is a global function and not a member function of a class.
     char buf[64] = {0};
-    ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof buf);
+    ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof buf); // 将网络字节序转换为字符串
     return buf;
 }
 
 std::string InetAddress::toIpPort() const
 {
-    // ip:port
+    // 字符串形式的ip:port
+    // The :: before the function name indicates that it is a global function and not a member function of a class.
     char buf[64] = {0};
-    ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof buf);
-    size_t end = strlen(buf);
-    uint16_t port = ntohs(addr_.sin_port);
-    sprintf(buf+end, ":%u", port);
+    ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof buf); // 将网络字节序转换为字符串
+    size_t end = strlen(buf);                               // 字符串的长度
+    uint16_t port = ntohs(addr_.sin_port);                  // 端口号
+    sprintf(buf + end, ":%u", port);                        // 将端口号加到字符串的末尾
     return buf;
 }
 
 uint16_t InetAddress::toPort() const
 {
-    return ntohs(addr_.sin_port);
+    return ntohs(addr_.sin_port); // 端口号
 }
 
 // #include <iostream>
--- InetAddress.h
+++ InetAddress.h
@@ -8,16 +8,21 @@
 class InetAddress
 {
 public:
+    // explicit 防止隐式转换
+    // 构造函数的两种形式
     explicit InetAddress(uint16_t port = 0, std::string ip = "127.0.0.1");
     explicit InetAddress(const sockaddr_in &addr) : addr_(addr) {}
 
-    std::string toIp() const; // 
-    std::string toIpPort() const;
-    uint16_t toPort() const;
+    std::string toIp() const;     // 返回字符串类型的 IP 地址
+    std::string toIpPort() const; // 返回字符串类型的 IP 地址和端口号
+    uint16_t toPort() const;      // 返回端口号
 
+    // 获取 socket address
     const sockaddr_in *getSockAddr() const { return &addr_; }
+
+    // 设置 socket address
     void setSockAddr(const sockaddr_in &addr) { addr_ = addr; }
 
 private:
-    sockaddr_in addr_; // IPv4 socket address
+    sockaddr_in addr_; // IPv4 socket address，这边只支持 IPv4
 };
