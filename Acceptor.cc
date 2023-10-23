1f7f33f321d07a164a2e36031a9ff38463367c70
supermicro <supermicro@scnu.com>
2023-10-23 22:39:07 +0800

    完善Acceptor类的注释

--- Acceptor.cc
+++ Acceptor.cc
@@ -2,32 +2,35 @@
 #include "Logger.h"
 #include "InetAddress.h"
 
-#include <sys/types.h>    
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <errno.h>
 #include <unistd.h>
 
-
 static int createNonblocking()
 {
+    // SOCK_STREAM表示TCP协议
+    // SOCK_NONBLOCK表示非阻塞
+    // SOCK_CLOEXEC表示在exec函数执行时，关闭该父进程的文件描述符
     int sockfd = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
     {
         LOG_FATAL("%s:%s:%d listen socket create err:%d \n", __FILE__, __FUNCTION__, __LINE__, errno);
     }
+    return sockfd;
 }
 
 Acceptor::Acceptor(EventLoop *loop, const InetAddress &listenAddr, bool reuseport)
-    : loop_(loop)
-    , acceptSocket_(createNonblocking()) // socket
-    , acceptChannel_(loop, acceptSocket_.fd())
-    , listenning_(false)
+    : loop_(loop),                              // mainLoop
+      acceptSocket_(createNonblocking()),       // 创建listenfd
+      acceptChannel_(loop, acceptSocket_.fd()), // 初始化listenfd对应的Channel
+      listenning_(false)                        // 是否正在监听
 {
     acceptSocket_.setReuseAddr(true);
     acceptSocket_.setReusePort(true);
     acceptSocket_.bindAddress(listenAddr); // bind
     // TcpServer::start() Acceptor.listen  有新用户的连接，要执行一个回调（connfd=》channel=》subloop）
-    // baseLoop => acceptChannel_(listenfd) => 
+    // baseLoop => acceptChannel_(listenfd) =>
     acceptChannel_.setReadCallback(std::bind(&Acceptor::handleRead, this));
 }
 
@@ -40,7 +43,7 @@ Acceptor::~Acceptor()
 void Acceptor::listen()
 {
     listenning_ = true;
-    acceptSocket_.listen(); // listen
+    acceptSocket_.listen();         // listen
     acceptChannel_.enableReading(); // acceptChannel_ => Poller
 }
 
@@ -53,7 +56,8 @@ void Acceptor::handleRead()
     {
         if (newConnectionCallback_)
         {
-            newConnectionCallback_(connfd, peerAddr); // 轮询找到subLoop，唤醒，分发当前的新客户端的Channel
+            // 轮询找到subLoop，唤醒，分发当前的新客户端的Channel
+            newConnectionCallback_(connfd, peerAddr); 
         }
         else
         {
@@ -63,7 +67,7 @@ void Acceptor::handleRead()
     else
     {
         LOG_ERROR("%s:%s:%d accept err:%d \n", __FILE__, __FUNCTION__, __LINE__, errno);
-        if (errno == EMFILE)
+        if (errno == EMFILE) // 文件描述符达到上限
         {
             LOG_ERROR("%s:%s:%d sockfd reached limit! \n", __FILE__, __FUNCTION__, __LINE__);
         }
--- Acceptor.h
+++ Acceptor.h
@@ -11,23 +11,28 @@ class InetAddress;
 class Acceptor : noncopyable
 {
 public:
-    using NewConnectionCallback = std::function<void(int sockfd, const InetAddress&)>;
+    
+    using NewConnectionCallback = std::function<void(int sockfd, const InetAddress &)>;
+    
     Acceptor(EventLoop *loop, const InetAddress &listenAddr, bool reuseport);
     ~Acceptor();
 
-    void setNewConnectionCallback(const NewConnectionCallback &cb) 
+    void setNewConnectionCallback(const NewConnectionCallback &cb)
     {
         newConnectionCallback_ = cb;
     }
 
     bool listenning() const { return listenning_; }
     void listen();
+
 private:
     void handleRead();
-    
-    EventLoop *loop_; // Acceptor用的就是用户定义的那个baseLoop，也称作mainLoop
-    Socket acceptSocket_;
-    Channel acceptChannel_;
-    NewConnectionCallback newConnectionCallback_;
-    bool listenning_;
+
+    EventLoop *loop_;       // Acceptor用的就是用户定义的那个baseLoop，也称作mainLoop
+    Socket acceptSocket_;   // listenfd
+    Channel acceptChannel_; // listenfd对应的Channel
+
+    NewConnectionCallback newConnectionCallback_; // 新连接来了，就执行这个回调，将新连接分配给subloop
+
+    bool listenning_; // 是否正在监听
 };
